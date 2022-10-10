d2e4a049e78f443db7133591e099b673825c9044
Ticoxxo <ticoxxo@gmail.com>
2022-10-09 19:36:26 -0600

    Deploy page

---
+++ build/precache-manifest.641d65e3acefb79da7feb40efdceb0df.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1d35ca67e91969de55a",
+    "url": "/artista/static/css/main.bf467896.chunk.css"
+  },
+  {
+    "revision": "c1d35ca67e91969de55a",
+    "url": "/artista/static/js/main.c1d35ca6.chunk.js"
+  },
+  {
+    "revision": "3da4e6a8535dd6d8027b",
+    "url": "/artista/static/js/1.3da4e6a8.chunk.js"
+  },
+  {
+    "revision": "b318b4edce87e5061240",
+    "url": "/artista/static/css/2.f079e222.chunk.css"
+  },
+  {
+    "revision": "b318b4edce87e5061240",
+    "url": "/artista/static/js/2.b318b4ed.chunk.js"
+  },
+  {
+    "revision": "c12ffddf557441f8107f",
+    "url": "/artista/static/js/runtime~main.c12ffddf.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/artista/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/artista/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/artista/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/artista/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9c5b5f2d10aefdd34775a18a9ba893fa",
+    "url": "/artista/static/media/1.9c5b5f2d.jpg"
+  },
+  {
+    "revision": "6ea8419fbe8f92cf2fe83c1903b9959d",
+    "url": "/artista/static/media/2.6ea8419f.jpg"
+  },
+  {
+    "revision": "27a3876419e2631e7b06b4998ccd2e79",
+    "url": "/artista/static/media/3.27a38764.jpg"
+  },
+  {
+    "revision": "0aaf07e53561c1beec997599ff5b64be",
+    "url": "/artista/static/media/4.0aaf07e5.jpg"
+  },
+  {
+    "revision": "efc64430057bc6eab1d231ec2579f3ca",
+    "url": "/artista/static/media/5.efc64430.jpg"
+  },
+  {
+    "revision": "e5d349c2855ac9798b3e26d5fcbc3a56",
+    "url": "/artista/static/media/6.e5d349c2.jpg"
+  },
+  {
+    "revision": "ff574da940b2d87dae10089481070ac3",
+    "url": "/artista/static/media/7.ff574da9.jpg"
+  },
+  {
+    "revision": "e15a5c4655cff36a608a3a0059f8b02c",
+    "url": "/artista/static/media/8.e15a5c46.jpg"
+  },
+  {
+    "revision": "0afa6e2b2a9630370bdea84a0d876935",
+    "url": "/artista/static/media/defaultImage.0afa6e2b.jpg"
+  },
+  {
+    "revision": "24b1e09d7d489a17ccf902e56b6226f9",
+    "url": "/artista/static/media/1.24b1e09d.jpg"
+  },
+  {
+    "revision": "238150bf35981fb6a2b536ac8617d7e3",
+    "url": "/artista/static/media/2.238150bf.jpg"
+  },
+  {
+    "revision": "51984cd9be1a2bc793633ab9174823bd",
+    "url": "/artista/static/media/3.51984cd9.jpg"
+  },
+  {
+    "revision": "1d8a68c04d9c2d310145d03769720fc1",
+    "url": "/artista/static/media/4.1d8a68c0.jpg"
+  },
+  {
+    "revision": "842f0c09197bb0292e336a2beb9f9f59",
+    "url": "/artista/static/media/5.842f0c09.jpg"
+  },
+  {
+    "revision": "837528599b63b25e8873ba3b32e239b0",
+    "url": "/artista/static/media/6.83752859.jpg"
+  },
+  {
+    "revision": "d4db08dedcb4a3982ca28c9977abd984",
+    "url": "/artista/static/media/7.d4db08de.jpg"
+  },
+  {
+    "revision": "25e2de03f63c8ad382729fbe49b489dd",
+    "url": "/artista/static/media/8.25e2de03.jpg"
+  },
+  {
+    "revision": "6095547d9368b3fa36589f38d2b97889",
+    "url": "/artista/index.html"
+  }
+];
