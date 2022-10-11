79f681fe4dec833ee1c8ef3189c5bd4e6a81b431
Ticoxxo <ticoxxo@gmail.com>
2022-10-10 21:38:53 -0600

    Redirect 404 page and gitignore

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/about" element={<About />} />
+      <Route path="*" element={<Home  />} />
       </Routes>
     </div>
   );
