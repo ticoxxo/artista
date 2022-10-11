bc5a3a58149a3e0faa87e77a75cbbdc3b278af4f
Ticoxxo <ticoxxo@gmail.com>
2022-10-10 21:12:21 -0600

    revert commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            <div className='about-child'>
+            <a href='https://www.facebook.com/Francisco.CardenasA'>Facebook</a>
+            </div>
+        </div>
+    )
+}
+
+export default About;
